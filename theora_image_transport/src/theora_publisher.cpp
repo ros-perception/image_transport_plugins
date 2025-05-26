6396e30057cbd3431fe57fcd4f14aa8fa713cd5d
Błażej Sowa <bsowa123@gmail.com>
2025-05-26 13:53:05 +0200

    Fix inconsistent transport param names
    Make sure the leading slash is removed when running the node under namespace, before replacing slashes with dots.

--- theora_image_transport/src/theora_publisher.cpp
+++ theora_image_transport/src/theora_publisher.cpp
@@ -154,7 +154,8 @@ void TheoraPublisher::advertiseImpl(
 
   // Declare Parameters
   uint ns_len = node->get_effective_namespace().length();
-  std::string param_base_name = base_topic.substr(ns_len);
+  uint ns_prefix_len = ns_len > 1 ? ns_len + 1 : ns_len;
+  std::string param_base_name = base_topic.substr(ns_prefix_len);
   std::replace(param_base_name.begin(), param_base_name.end(), '/', '.');
 
   using callbackT = std::function<void(ParameterEvent::SharedPtr event)>;
